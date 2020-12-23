52999332e614a4a7b653cc94b9ccef2b396a5d1a
explorer-pan <1076230322@qq.com>
2020-12-23 13:28:57 +0800

    Implement Main

--- GTE/Samples/CSharpCppManaged/CSharpApplication/Program.cs
+++ GTE/Samples/CSharpCppManaged/CSharpApplication/Program.cs
@@ -28,51 +28,13 @@ namespace CSharpApplication
     {
         static void Main(string[] args)
         {
-            int numPoints = 2048;
-            double[] points = new double[3 * numPoints];
-
-            double[] center = new double[] { 0.0, 0.0, 0.0 };
-            double[] extent = new double[] { 1.0, 0.25, 0.125 };
-            double sqrtHalf = Math.Sqrt(0.5);
-            double[] axis = new double[] {
-                sqrtHalf, sqrtHalf, 0.0,
-                -sqrtHalf, sqrtHalf, 0.0,
-                0.0, 0.0, 1.0 };
-            double[] volume = new double[] { 0.0 };
-
-            Random generator = new Random();
-
-            for (int i = 0; i < numPoints; ++i)
+            var points = new double[]
             {
-                double rnd = 2.0 * generator.NextDouble() - 1.0;
-                double theta = rnd * 2.0 * Math.PI;
-                rnd = 2.0 * generator.NextDouble() - 1.0;
-                double phi = rnd * Math.PI;
-                double radius = generator.NextDouble();
-                double x = extent[0] * Math.Cos(theta) * Math.Sin(phi);
-                double y = extent[1] * Math.Sin(theta) * Math.Sin(phi);
-                double z = extent[2] * Math.Cos(phi);
-                for (int j = 0; j < 3; ++j)
-                {
-                    points[3 * i + j] = center[j] +
-                        radius * (x * axis[j] + y * axis[j + 3] + z * axis[j + 6]);
-                }
-            }
-
-            MVB3 mvb = new MVB3();
-            uint numThreads = 4;
-            uint lgMaxSample = 10;
-            mvb.ComputeMinimumVolumeBoxFromPoints(numThreads, numPoints, points,
-                lgMaxSample, center, axis, extent, volume);
-
-            Console.WriteLine("center  = " + center[0] + ", " + center[1] + ", " + center[2]);
-            Console.WriteLine("extent  = " + extent[0] + ", " + extent[1] + ", " + extent[2]);
-            Console.WriteLine("axis[0] = " + axis[0] + ", " + axis[1] + ", " + axis[2]);
-            Console.WriteLine("axis[1] = " + axis[3] + ", " + axis[4] + ", " + axis[5]);
-            Console.WriteLine("axis[2] = " + axis[6] + ", " + axis[7] + ", " + axis[8]);
-            Console.WriteLine("volume  = " + volume[0]);
-            Console.WriteLine("Press enter to exit program.");
-            Console.Read();
+                0,0, 100,0, 100,100, 0,100,  20,20, 80,20, 80,80, 20,80
+            };
+            var indices = new int[] { 4 };
+            var builder = new ThEarCutTriangulationBuilder();
+            builder.EarCut(points, 8, indices, 1);
         }
     }
 }
