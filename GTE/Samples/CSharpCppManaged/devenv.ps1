3d016347fbf8ba980fe2d820e5bf88ce62795fa6
Jerry Dong <shichongdong80@163.com>
2021-04-02 23:18:13 +0800

    add devenv.ps1

---
+++ GTE/Samples/CSharpCppManaged/devenv.ps1
@@ -0,0 +1,41 @@
+function Get-LatestMsbuildLocation
+{
+  Param 
+  (
+    [bool] $allowPreviewVersions = $false
+  )
+    if ($allowPreviewVersions) {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All -Prerelease | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    } else {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    }
+    Write-Host "Latest version installed is $($latestVsInstallationInfo.InstallationVersion)"
+    if ($latestVsInstallationInfo.InstallationVersion -like "15.*") {
+      $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\15.0\Bin\msbuild.exe"
+    
+      Write-Host "Located msbuild for Visual Studio 2017 in $msbuildLocation"
+    } else {
+      $msbuildLocation = "$($latestVsInstallationInfo.InstallationPath)\MSBuild\Current\Bin\msbuild.exe"
+      Write-Host "Located msbuild in $msbuildLocation"
+    }
+
+    return $msbuildLocation
+}
+function Get-LatestDevenvLocation
+{
+  Param 
+  (
+    [bool] $allowPreviewVersions = $false
+  )
+    if ($allowPreviewVersions) {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All -Prerelease | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    } else {
+      $latestVsInstallationInfo = Get-VSSetupInstance -All | Sort-Object -Property InstallationVersion -Descending | Select-Object -First 1
+    }
+    Write-Host "Latest version installed is $($latestVsInstallationInfo.InstallationVersion)"
+
+    $devenvLocation = "$($latestVsInstallationInfo.InstallationPath)\Common7\IDE\devenv.exe"
+    Write-Host "Located devenv in $devenvLocation"
+
+    return $msbuildLocation
+}
