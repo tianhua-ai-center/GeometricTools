e5d03a794f2a60cf7f7c2b604433e329c65c03be
Jerry Dong <shichongdong80@163.com>
2021-04-28 14:10:11 +0800

    Init CppApplication project

---
+++ GTE/Samples/CSharpCppManaged/CppApplication/CppApplication.cpp
@@ -0,0 +1,10 @@
+// CppApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	return 0;
+}
