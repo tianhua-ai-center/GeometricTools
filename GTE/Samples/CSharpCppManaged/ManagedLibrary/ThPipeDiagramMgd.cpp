6e869d90312b67b8533de074ef6e9917069c6b38
董士崇 <dongshichong@thape.com.cn>
2021-07-07 18:19:37 +0800

    Clean comment code

--- GTE/Samples/CSharpCppManaged/ManagedLibrary/ThPipeDiagramMgd.cpp
+++ GTE/Samples/CSharpCppManaged/ManagedLibrary/ThPipeDiagramMgd.cpp
@@ -1,51 +1,25 @@
-#include <string>
 #include "ThPipeDiagramMgd.h"
 #include "ThWashInteropUtils.h"
-#include <region.h>
-#include <grouping.h>
+#include "GroupingPipe/include/region.h"
+#include "GroupingPipe/include/grouping.h"
+
 using namespace GroupingPipe;
+
 namespace CLI
 {
-	/*String^ ThPipeSystemDiagramMgd::ProcessGroupingData(String^ input)
-	{
-		std::string strNativeInput;
-		MarshalString(input, strNativeInput);
-
-		std::string strNativeOutput = grouping_geojson_string(strNativeInput);
-
-		auto output = MarshalNativeString(strNativeOutput);
-
-		return output;
-	}*/
-
-	//String^ ThPipeSystemDiagramMgd::ProcessGroupesMerge(String^ input)
-	//{
-	//	//todo:
-	//	return nullptr;
-	//}
-
 	String^ ThPipeSystemDiagramMgd::ProcessMainBranchs(String^ input)
 	{
 		std::string strNativeInput;
 		MarshalString(input, strNativeInput);
-
 		std::string strNativeOutput = process_region_by_string(strNativeInput);
-
-		auto output = MarshalNativeString(strNativeOutput);
-
-		return output;
+		return MarshalNativeString(strNativeOutput);
 	}
 
 	String^ ThPipeSystemDiagramMgd::ProcessGrouping(String^ input)
 	{
 		std::string strNativeInput;
 		MarshalString(input, strNativeInput);
-
 		string strNativeOutput = grouping_geojson_string(strNativeInput);
-		//std::string strNativeOutput = process_region_by_string(strNativeInput);
-
-		auto output = MarshalNativeString(strNativeOutput);
-
-		return output;
+		return MarshalNativeString(strNativeOutput);
 	}
 }
