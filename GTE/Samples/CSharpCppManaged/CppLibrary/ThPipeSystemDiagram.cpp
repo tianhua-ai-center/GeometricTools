192fc00e9db6a5b6e858fb647dd4e570718177a6
董士崇 <dongshichong@thape.com.cn>
2021-07-10 22:04:21 +0800

    Export ThPipeSystemDiagramMgd class

---
+++ GTE/Samples/CSharpCppManaged/CppLibrary/ThPipeSystemDiagram.cpp
@@ -0,0 +1,15 @@
+#include "ThPipeSystemDiagram.h"
+#include "GroupingPipe/include/region.h"
+#include "GroupingPipe/include/grouping.h"
+
+using namespace gte;
+
+std::string ThPipeSystemDiagram::ProcessGrouping(const std::string& geojson)
+{
+    return GroupingPipe::process_region_by_string(geojson);
+}
+
+std::string ThPipeSystemDiagram::ProcessMainBranchs(const std::string& geojson)
+{
+    return GroupingPipe::process_region_by_string(geojson);
+}
